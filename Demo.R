4398ef8b90d498909fd46e762010193671a6f5fd
holgstr <hbj.loewe@gmail.com>
2022-05-01 04:48:09 +0200

    adds helper functions for Predictortest Class

--- Demo.R
+++ Demo.R
@@ -2,6 +2,7 @@
 library("R6")
 library("data.table")
 library("checkmate")
+library("mlr3verse")
 
 #### Packages for Demo Purposes ------------------------------------------------------------------
 library("iml")
@@ -15,25 +16,42 @@ sapply(files[which(files != "Demo.R")], source)
 set.seed(123)
 data("Boston", package = "MASS")
 Boston$chas = as.factor(Boston$chas)
-forest = randomForest(medv ~ ., data = Boston)
-predictor = Predictor$new(forest, data = Boston)
 
-#eff <- FeatureEffect$new(predictor, feature = "rm", grid.size = 30, method = "pdp+ice")
-#plot(eff)
+
+
+
+### Example 1 --------------------------------------
+forest = randomForest(medv ~ ., data = Boston)
+predictor1 = Predictor$new(forest, data = Boston)
 
 # Example categorical step
 a = ForwardMarginalEffect$new(feature = c("chas"),
-                          predictor = predictor,
+                          predictor = predictor1,
                           step.size = "0",
                           ep.method = "envelope", # atm envelope is only checked for numerical features
                           nlm.intervals = 1)
 #a$results
 
 # Example numerical step
-b = ForwardMarginalEffect$new(feature = c("ptratio", "tax"),
-                              predictor = predictor,
-                              step.size = c(3, 100),
+b = ForwardMarginalEffect$new(feature = c("rm", "tax"),
+                              predictor = predictor1,
+                              step.size = c(2, 100),
                               ep.method = "envelope",
                               nlm.intervals = 1)
-b$results
+#b$results
 
+
+
+
+### Example 2 --------------------------------------
+task = as_task_regr(Boston, id = "BostonHousing", target = "medv")
+learner = lrn("regr.rpart")
+learner$train(task)
+Boston = as.data.table(Boston)
+predictor2= Predictortest$new(model = learner, data = Boston, y = "medv")
+
+# so far only test version of my own predictor class
+predictor2$predict(Boston)
+predictor2$feature.types
+predictor2$feature.names
+predictor2$X
--- Predictortest.R
+++ Predictortest.R
@@ -1,17 +1,25 @@
 Predictortest <- R6Class("Predictortest",
   public = list(
     initialize = function(model, data, y) {
-    
+      
+      # Check if data is data.frame
+      assert_data_frame(data, all.missing = FALSE)
+      
+      # Transform data into data.table if neccesary
+      if (test_data_table(data)) {
+        data = as.data.table(data)
+      }
+      
       self$model = model
       self$feature.names = private$get.feature.names(data, y)
       self$X = private$get.X(data, self$feature.names)
-      self$feature.types = private$get.feature.types(self$X)
+      self$feature.types = private$get.feature.types(self$X, self$feature.names)
       
     },
     
+    # Predict function with current support for mlr3 models
     predict = function(newdata) {
-      # function that calls self$model and predicts on new observations
-      as.data.table(self$model$predict_newdata(Boston[1:6,]))[,3]
+      return(as.data.table(self$model$predict_newdata(newdata))[,3])
     },
     
     model = NULL,
@@ -22,20 +30,22 @@ Predictortest <- R6Class("Predictortest",
   private = list(
     
     get.X = function(data, feature.names) {
-      #a$data$X
-      data[feature.names]
+      return(data[, ..feature.names])
     },
     
     get.feature.names = function(data, y) {
-      # function that gets feature names
-      #a$data$feature.names
-      setdiff(names(data), y)
+      return(setdiff(names(data), y))
     },
     
-    get.feature.types = function(X) {
-      # function that gets feature types
-      #a$data$feature.types
-      4
+    get.feature.types = function(X, feature.names) {
+      feature.types <- c("integer" = "numerical",
+                         "numeric" = "numerical",
+                         "character" = "categorical",
+                         "factor" = "categorical",
+                         "ordered" = "categorical")
+      feature.types = feature.types[unlist(lapply(X, function(x){class(x)[1]}))]
+      names(feature.types) = feature.names
+      return(feature.types)
     }
   
   )
