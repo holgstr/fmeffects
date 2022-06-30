48901acd8c32245d258bfb1a6d9503fbb697b8e7
holgstr <hbj.loewe@gmail.com>
2022-07-01 01:02:11 +0200

    S3 updated

--- R/S3.R
+++ R/S3.R
@@ -1,8 +1,20 @@
 summary.Partitioning = function(partitioning) {
-  #ans = length(nodeids(partitioning$tree, terminal = TRUE))
-  #class(ans) = "summary.Partitioning"
-  #ans
-  partitioning
+  ans = partitioning$results
+  ans
 }
 
+print.Partitioning = function(partitioning) {
+  cat("\n")
+  cat(class(partitioning)[1])
+  cat(" of an FME object\n\n")
+  cat("Method:  ")
+  cat(partitioning$method)
+  cat(" = ")
+  cat(partitioning$value)
+  cat("\n\n")
+  print(do.call(rbind.data.frame, f$results))
+}
 
+plot.Partitioning = function(partitioning) {
+  plot(partitioning$tree)
+}
