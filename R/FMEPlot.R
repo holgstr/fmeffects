56f6d7be9e2ae16f6c846fc6690c18511b8c738a
holgstr <hbj.loewe@gmail.com>
2022-07-16 22:01:54 +0200

    adds FMEPlotCategorical, adds self$ references

--- R/FMEPlot.R
+++ R/FMEPlot.R
@@ -24,8 +24,7 @@ FMEPlot = R6Class("FMEPlot",
       results = data.table::copy(results)
       add = data[, .SD, .SDcols = feature]
       add = add[i = results$obs.id]
-      results = cbind(results, add)
-      self$df = results
+      self$df = cbind(results, add)
 
     }
   )
@@ -45,9 +44,9 @@ FMEPlotBivariate = R6Class("FMEPlotBivariate",
 
     plot = function(with.nlm = FALSE) {
       df = as.data.frame(self$df)
-      x1 = df[,which(feature[1] == names(df))]
+      x1 = df[,which(self$feature[1] == names(df))]
       range.x1 = diff(range(x1))
-      x2 = df[,which(feature[2] == names(df))]
+      x2 = df[,which(self$feature[2] == names(df))]
       range.x2 = diff(range(x2))
 
       pfme = ggplot(df) +
@@ -56,22 +55,22 @@ FMEPlotBivariate = R6Class("FMEPlotBivariate",
                    shape = 21,
                    alpha = 0.7) +
         scale_fill_viridis_c(guide_legend("fME")) +
-        geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * step.size[1]),
-                         xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * step.size[1]),
+        geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * self$step.size[1]),
+                         xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * self$step.size[1]),
                          y = min(x2)-0.03*range.x2,
                          yend = min(x2)-0.03*range.x2),
                      colour = 'black', size = 1,
                      arrow = arrow(length = unit(0.5, "cm")),
                      lineend = "round", linejoin = "mitre") +
-        geom_segment(aes(y = (0.5 * min(x2) + 0.5 * max(x2) - 0.5 * step.size[2]),
-                         yend = (0.5 * min(x2) + 0.5 * max(x2) + 0.5 * step.size[2]),
+        geom_segment(aes(y = (0.5 * min(x2) + 0.5 * max(x2) - 0.5 * self$step.size[2]),
+                         yend = (0.5 * min(x2) + 0.5 * max(x2) + 0.5 * self$step.size[2]),
                          x = min(x1)-0.03*range.x1,
                          xend = min(x1)-0.03*range.x1),
                      colour = 'black', size = 1,
                      arrow = arrow(length = unit(0.5, "cm")),
                      lineend = "round", linejoin = "mitre") +
-        xlab(feature[1]) +
-        ylab(feature[2]) +
+        xlab(self$feature[1]) +
+        ylab(self$feature[2]) +
         theme_bw() +
         theme(panel.border = element_rect(colour = "black", fill=NA, size=0.7),
               axis.title = element_text(size = 12),
@@ -92,19 +91,19 @@ FMEPlotBivariate = R6Class("FMEPlotBivariate",
           scale_fill_viridis_c(guide_legend("NLM"),
                                breaks=c(0.98, 0.5, 0.01),
                                labels = c("1.0", "0.5", "\u2264 0")) +
-          geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * step.size[1]),
-                           xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * step.size[1]),
+          geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * self$step.size[1]),
+                           xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * self$step.size[1]),
                            y = min(x2)-0.03*range.x2, yend = min(x2)-0.03*range.x2),
                        colour = 'black', size = 1, arrow = arrow(length = unit(0.5, "cm")),
                        lineend = "round", linejoin = "mitre") +
-          geom_segment(aes(y = (0.5 * min(x2) + 0.5 * max(x2) - 0.5 * step.size[2]),
-                           yend = (0.5 * min(x2) + 0.5 * max(x2) + 0.5 * step.size[2]),
+          geom_segment(aes(y = (0.5 * min(x2) + 0.5 * max(x2) - 0.5 * self$step.size[2]),
+                           yend = (0.5 * min(x2) + 0.5 * max(x2) + 0.5 * self$step.size[2]),
                            x = min(x1)-0.03*range.x1, xend = min(x1)-0.03*range.x1),
                        colour = 'black', size = 1,
                        arrow = arrow(length = unit(0.5, "cm")),
                        lineend = "round", linejoin = "mitre") +
-          xlab(feature[1]) +
-          ylab(feature[2]) +
+          xlab(self$feature[1]) +
+          ylab(self$feature[2]) +
           theme_bw() +
           theme(panel.border = element_rect(colour = "black", fill=NA, size=0.7),
                 axis.title = element_text(size = 12),
@@ -133,7 +132,8 @@ FMEPlotUnivariate = R6Class("FMEPlotUnivariate",
 
     plot = function(with.nlm = FALSE) {
       df = as.data.frame(self$df)
-      x1 = df[,which(feature == names(df))]
+      x1 = df[,which(self$feature == names(df))]
+      range.x1 = diff(range(x1))
       range.fme = diff(range(df$fme))
 
       pfme = ggplot(df) +
@@ -143,14 +143,16 @@ FMEPlotUnivariate = R6Class("FMEPlotUnivariate",
                    size = 3.8,
                    shape = 21,
                    alpha = 0.55) +
-        geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * step.size[1]),
-                         xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * step.size[1]),
+        geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * self$step.size[1]),
+                         xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * self$step.size[1]),
                          y = min(fme)-0.03*range.fme,
                          yend = min(fme)-0.03*range.fme),
                      colour = 'black', size = 1,
                      arrow = arrow(length = unit(0.5, "cm")),
                      lineend = "round", linejoin = "mitre") +
-        xlab(feature[1]) +
+        geom_hline(lwd = 1.2, mapping = aes(yintercept = mean(fme))) +
+        geom_label(x = max(x1) - 0.05 * range.x1, y = mean(df$fme), label = 'AME', fill = 'white') +
+        xlab(self$feature[1]) +
         ylab("fME") +
         theme_bw() +
         theme(panel.border = element_rect(colour = "black", fill=NA, size=0.7),
@@ -163,6 +165,7 @@ FMEPlotUnivariate = R6Class("FMEPlotUnivariate",
       if (with.nlm == FALSE) {
         pfme
       } else if ("nlm" %in% names(df)) {
+        meannlm = mean(df$nlm, na.rm = TRUE)
         df$nlm = sapply(df$nlm, FUN = function(x) {max(x, 0)})
         range.nlm = diff(range(df$nlm))
         pnlm = ggplot(df) +
@@ -172,14 +175,16 @@ FMEPlotUnivariate = R6Class("FMEPlotUnivariate",
                      shape = 21,
                      alpha = 0.55,
                      show.legend = FALSE) +
-          geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * step.size[1]),
-                           xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * step.size[1]),
+          geom_segment(aes(x = (0.5 * min(x1) + 0.5 * max(x1) - 0.5 * self$step.size[1]),
+                           xend = (0.5 * min(x1) + 0.5 * max(x1) + 0.5 * self$step.size[1]),
                            y = min(nlm)-0.03*range.nlm,
                            yend = min(nlm)-0.03*range.nlm),
                        colour = 'black', size = 1,
                        arrow = arrow(length = unit(0.5, "cm")),
                        lineend = "round", linejoin = "mitre") +
-          xlab(feature) +
+          geom_hline(lwd = 1.2, mapping = aes(yintercept = meannlm)) +
+          geom_label(x = max(x1) - 0.05 * range.x1, y = meannlm, label = 'ANLM', fill = 'white') +
+          xlab(self$feature) +
           ylab("NLM") +
           theme_bw() +
           scale_fill_manual(values = c("aliceblue", "cornflowerblue")) +
@@ -193,6 +198,47 @@ FMEPlotUnivariate = R6Class("FMEPlotUnivariate",
       } else {
         stop("Only possible to plot NLM for FME objects with NLM computed.")
       }
+    }
+  )
+)
+
+# FMEPlot for Categorical Steps
+FMEPlotCategorical = R6Class("FMEPlotCategorical",
+
+  inherit = FMEPlot,
+
+  public = list(
+
+    initialize = function(results, data, feature, step.size) {
+      private$initializeSubclass(results, data, feature, step.size)
+    },
+
+    plot = function() {
+      df = as.data.frame(self$df)
+      countmax = max(hist(df$fme,
+                          breaks = seq(min(df$fme),
+                                       max(df$fme),
+                                       l=min(round(nrow(df))*0.4, 20)+1),
+                          plot = FALSE)$counts)
+      ggplot(df) +
+        geom_histogram(lwd = 0.3,
+                       linetype = "solid",
+                       colour = "black",
+                       fill = "cornflowerblue",
+                       show.legend = FALSE,
+                       mapping = aes(x = fme, y = ..count..),
+                       bins = min(round(nrow(df))*0.4, 20),
+                       na.rm = TRUE) +
+        geom_density(mapping = aes(x = fme, y = ..scaled..*countmax)) +
+        geom_vline(lwd = 1.2, mapping = aes(xintercept = mean(fme))) +
+        geom_label(x = mean(df$fme), y = countmax*0.9, label = 'AME', fill = 'white') +
+        xlab(paste0("fME (category: ", self$step.size, ", feature: ", self$feature, ")")) +
+        ylab("") +
+        theme_bw() +
+        theme(panel.border = element_rect(colour = "black", fill=NA, size=0.7),
+              axis.title = element_text(size = 12),
+              axis.text.x   = element_text(colour = "black", size = 10),
+              axis.text.y   = element_text(colour = "black", size = 10))
     }
   )
 )
