8b0ba6c3a6ad18786b80dfa99ba1083783742479
holgstr <hbj.loewe@gmail.com>
2022-07-26 01:36:07 +0200

    updates Demo

--- inst/Demo.R
+++ inst/Demo.R
@@ -2,21 +2,23 @@
 # DEMO: INTERPRETING FORWARD MARGINAL EFFECTS ON WASHINGTON BIKESHARE DATA
 #--------------------------------------------------------------------------
 
+set.seed(123)
+
 # DATA --------------------------------------------------------------------
 
 # Import data from the OpenML database
 require(OpenML)
 require(farff)
-miami = as.data.table(getOMLDataSet(data.id = 42712)$data)
-miami = miami[,-c(10,13,15)]
-miami = miami[hour %inrange% c(15,17)]
-miami = miami[year == 1]
-miami = miami[, -c(2)]
+Bikes = as.data.table(getOMLDataSet(data.id = 42712)$data)
+Bikes = Bikes[,-c(10,13,14)]
+Bikes = Bikes[hour %inrange% c(7,8)]
+Bikes = Bikes[year == 1]
+Bikes = Bikes[, -c(2)]
 
 # TRAIN MODEL -------------------------------------------------------------
 
 # Use an
-task = as_task_regr(miami, id = "miami", target = "registered")
-forest = lrn("regr.ranger", num.trees = 400, mtry= 5, importance = "permutation")$train(task)
+task = as_task_regr(x = Bikes, id = "Bikes", target = "count")
+forest = lrn("regr.ranger")$train(task)
 forest$model
 
