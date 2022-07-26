3051552c7788fcdb5e0309a920dfd621ecb8e6c2
holgstr <hbj.loewe@gmail.com>
2022-07-26 18:26:03 +0200

    test

--- R/FME.R
+++ R/FME.R
@@ -275,11 +275,11 @@ FME = R6Class("FME",
 #'
 #' @export
 fme = function(model, data, target, feature, step.size, ep.method = "none", compute.nlm = FALSE, nlm.intervals = 1) {
+  print("Test")
   return(FME$new(makePredictor(model, data, target),
           feature = feature,
           step.size = step.size,
           ep.method = ep.method,
           compute.nlm = compute.nlm,
           nlm.intervals = nlm.intervals)$compute())
-  print("Test")
 }
