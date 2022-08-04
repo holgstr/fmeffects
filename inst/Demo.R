e35177edb29d2fc715d7a7bf8c1e54d69c38fa8b
holgstr <hbj.loewe@gmail.com>
2022-08-04 21:58:33 +0200

    update Demo

--- inst/Demo.R
+++ inst/Demo.R
@@ -11,6 +11,7 @@ install_github("holgstr/fme", force = TRUE, upgrade = "never")
 library(fme)
 
 
+
 ##### DATA --------------------------------------------------------------------
 
 # Import data from the OpenML database
@@ -33,6 +34,7 @@ forest = lrn("regr.ranger")$train(task)
 forest$model
 
 
+
 #### COMPUTE MARGINAL EFFECTS -------------------------------------------------
 
 # In order to adapt the model to fme, one can use the R6 syntax:
@@ -43,6 +45,9 @@ pred = makePredictor(model = forest, data = bikes, target = "count")
 
 # However, computing fMEs effectively requires only the fme() funtion:
 ?fme
+# !!! If you have just installed fme in the same RStudio session, this might show "internal server error" !!!
+# !!! In this case, restart the session and simply reload fme via 'library(fme)' !!!
+
 
 
 ### CATEGORICAL FEATURES ------------------------------------------------------
@@ -51,7 +56,7 @@ pred = makePredictor(model = forest, data = bikes, target = "count")
 effects = fme(model = forest,
               data = bikes,
               target = "count",
-              feature = "weather",
+              feature = "hour",
               step.size = "rain")
 
 # We have created an object of class 'FME':
@@ -69,8 +74,6 @@ head(effects$results)
 # Finally, we visualize the MEs with plot():
 plot(effects)
 
-#p = plot(effects)
-#ggsave("weather_rain.pdf", p, units = "cm", width = 12.5, height = 8.4)
 
 
 ### NUMERICAL FEATURES --------------------------------------------------------
@@ -94,11 +97,6 @@ effects2$anlm
 # We include NLMs and jitter the points (see ?geom_jitter) to avoid overlapping:
 plot(effects2, with.nlm = TRUE, jitter = c(0.2, 0))
 
-#p2 = plot(effects2, with.nlm = TRUE, jitter = c(0.2, 0))
-#grDevices::cairo_pdf("temp_3.pdf", width = 7.28, height = 3.31)
-#p2
-#dev.off()
-
 # Compute fMEs for the num. features "temp" and "humidity", with step sizes -2, -0.1
 # This corresponds to a temperature decrease of 2 degrees celsius and a decrease in humidity by 10 percentage points:
 effects3 = fme(model = forest,
@@ -113,10 +111,6 @@ effects3 = fme(model = forest,
 # We include NLMs and jitter the points (see ?geom_jitter) to avoid overlapping:
 plot(effects3, with.nlm = TRUE, jitter = c(0.02, 0.02))
 
-#p3 = plot(effects3, with.nlm = TRUE, jitter = c(0.2, 0.02))
-#grDevices::cairo_pdf("temphumid.pdf", width = 9, height = 3.51)
-#p3
-#dev.off()
 
 
 ### SEMI-GLOBAL INTERPRETATIONS -----------------------------------------------
@@ -131,11 +125,6 @@ summary(subspaces)
 # Finally, we visualize the partitioning with plot()
 plot(subspaces)
 
-#p4 = plot(subspaces)
-#grDevices::cairo_pdf("subspaces.pdf", width = 9, height = 6)
-#p4
-#dev.off()
-
 # Let us assume we want to find a partitioning for 'effects',
 # i.e., the categorical feature change of 'weather' to 'rain'
 subspaces2 = came(effects = effects,
@@ -146,19 +135,8 @@ subspaces2 = came(effects = effects,
 # We visualize the partitioning with plot()
 plot(subspaces2)
 
-#p5 = plot(subspaces2)
-#grDevices::cairo_pdf("subspaces2.pdf", width = 6, height = 4.5)
-#p5
-#dev.off()
-
 # In fme, we use assertions to ensure a user make sensible inputs:
 came(effects = effects2, max.cov = 1.5, number.partitions = 8)
 
 # A partitioning with 8 partitions can be created and plotted with:
 plot(came(effects = effects2, number.partitions = 8))
-
-#p6 = plot(came(effects = effects2, number.partitions = 8))
-#grDevices::cairo_pdf("subspaces3.pdf", width = 16, height = 8)
-#p6
-#dev.off()
-
